3cdd15a2818979740153c13435c0ecff75dc4e18
piotr.chmielinski <piotr.chmielinski@dynatrace.com>
2025-10-24 11:40:12 +0200

    cluster: pods order changed for 1st deployment
    creating new cluster: start with 1 node/rack, then scale-out to desired size
    
    Fixes #180

--- controllers/cassandracluster/cassandra_status.go
+++ controllers/cassandracluster/cassandra_status.go
@@ -480,6 +480,7 @@ func (rcc *CassandraClusterReconciler) UpdateCassandraRackStatusPhase(ctx contex
 		}
 		if len(podsList.Items) < int(nodesPerRacks) {
 			logrus.WithFields(logrusFields).Infof("StatefulSet is scaling up")
+			return
 		}
 		pod := podsList.Items[nodesPerRacks-1]
 		if cassandraPodIsReady(&pod) {
@@ -501,6 +502,33 @@ func (rcc *CassandraClusterReconciler) UpdateCassandraRackStatusPhase(ctx contex
 		logrus.WithFields(logrusFields).Infof("StatefulSet: Rack Phase is not %s", api.ClusterPhaseRunning.Name)
 		status.CassandraRackStatus[dcRackName].SetRunningPhase()
 		ClusterPhaseMetric.set(api.ClusterPhaseRunning, cc.Name)
+	}
+}
+
+// UpdateCassandraRackStatusFirstPodPerRackInitPhase goal is to calculate the Cluster Subphase of Phase Initializing according to StatefulSet Status.
+// The Subphase goes one way: FirstPodPerRack -> NextPodPerRack
+func (rcc *CassandraClusterReconciler) UpdateCassandraRackStatusFirstPodPerRackInitPhase(ctx context.Context, cc *api.CassandraCluster, dcName string,
+	rackName string, storedStatefulSet *appsv1.StatefulSet, status *api.CassandraClusterStatus) {
+	dcRackName := cc.GetDCRackName(dcName, rackName)
+
+	logrusFields := logrus.Fields{"cluster": cc.Name, "rack": dcRackName, "phase": status.CassandraRackStatus[dcRackName].CassandraPhase,
+		"ReadyReplicas": storedStatefulSet.Status.ReadyReplicas, "RequestedReplicas": *storedStatefulSet.Spec.Replicas}
+
+	ClusterPhaseMetric.set(api.ClusterPhaseInitial, cc.Name)
+
+	if isStatefulSetNotReady(storedStatefulSet) {
+		logrus.WithFields(logrusFields).Infof("Initializing StatefulSet: Replicas count is not okay")
+		return
+	}
+	//If yes, just check that lastPod is running
+	podsList, err := rcc.ListPods(ctx, cc.Namespace, k8s.LabelsForCassandraDCRack(cc, dcName, rackName))
+	if err != nil || len(podsList.Items) < 1 {
+		return
+	}
+	pod := podsList.Items[len(podsList.Items)-1]
+	if cassandraPodIsReady(&pod) {
+		status.CassandraRackStatus[dcRackName].SetNextPodPerRackInitPhase()
+		logrus.WithFields(logrusFields).Infof("StatefulSet: Replicas count is okay")
 	}
 }
 
